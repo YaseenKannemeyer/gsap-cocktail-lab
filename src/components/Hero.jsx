841fde893809eeb17a08eb6ade926ccea34684b0
mogamatyaseenkannemeyer <yaseenkannemeyer@gmail.com>
2026-04-06 16:20:35 +0200

    feat: add Hero component to the App for enhanced landing page visuals

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title"> MOJITO</h1>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
