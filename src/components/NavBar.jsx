82c01e4104c29b6cdc74d2760b6cf19641bc5192
mogamatyaseenkannemeyer <yaseenkannemeyer@gmail.com>
2026-04-06 16:54:13 +0200

    feat: implement video background and responsive animations in Hero component; refactor NavBar GSAP animations

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -21,17 +21,6 @@ const NavBar = () => {
         ease: "power1.inOut",
       },
     );
-    gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: "#hero",
-          start: "top top",
-          end: "bottom top",
-          scrub: true,
-        },
-      })
-      .to(".right-leaf", { y: 200 }, 0)
-      .to(".right-leaf", { y: -200 }, 0);
   }, []);
   return (
     <nav>
